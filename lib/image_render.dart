4e3e0a9cac793a8b0d9c60af7a9478ebce2f7519
黄荣烽 <huangrongfeng@yingyinglicai.com>
2022-05-16 11:26:13 +0800

    save

--- lib/image_render.dart
+++ lib/image_render.dart
@@ -13,12 +13,17 @@ typedef ImageSourceMatcher = bool Function(
   dom.Element? element,
 );
 
-final _dataUriFormat = RegExp("^(?<scheme>data):(?<mime>image\/[\\w\+\-\.]+)(?<encoding>;base64)?\,(?<data>.*)");
+final _dataUriFormat = RegExp(
+    "^(?<scheme>data):(?<mime>image\/[\\w\+\-\.]+)(?<encoding>;base64)?\,(?<data>.*)");
 
-ImageSourceMatcher dataUriMatcher({String? encoding = 'base64', String? mime}) => (attributes, element) {
+ImageSourceMatcher dataUriMatcher(
+        {String? encoding = 'base64', String? mime}) =>
+    (attributes, element) {
       if (_src(attributes) == null) return false;
       final dataUri = _dataUriFormat.firstMatch(_src(attributes)!);
-      return dataUri != null && (mime == null || dataUri.namedGroup('mime') == mime) && (encoding == null || dataUri.namedGroup('encoding') == ';$encoding');
+      return dataUri != null &&
+          (mime == null || dataUri.namedGroup('mime') == mime) &&
+          (encoding == null || dataUri.namedGroup('encoding') == ';$encoding');
     };
 
 ImageSourceMatcher networkSourceMatcher({
@@ -30,13 +35,16 @@ ImageSourceMatcher networkSourceMatcher({
       if (_src(attributes) == null) return false;
       try {
         final src = Uri.parse(_src(attributes)!);
-        return schemas.contains(src.scheme) && (domains == null || domains.contains(src.host)) && (extension == null || src.path.endsWith(".$extension"));
+        return schemas.contains(src.scheme) &&
+            (domains == null || domains.contains(src.host)) &&
+            (extension == null || src.path.endsWith(".$extension"));
       } catch (e) {
         return false;
       }
     };
 
-ImageSourceMatcher assetUriMatcher() => (attributes, element) => _src(attributes) != null && _src(attributes)!.startsWith("asset:");
+ImageSourceMatcher assetUriMatcher() => (attributes, element) =>
+    _src(attributes) != null && _src(attributes)!.startsWith("asset:");
 
 typedef ImageRender = Widget? Function(
   RenderContext context,
@@ -45,7 +53,8 @@ typedef ImageRender = Widget? Function(
 );
 
 ImageRender base64ImageRender() => (context, attributes, element) {
-      final decodedImage = base64.decode(_src(attributes)!.split("base64,")[1].trim());
+      final decodedImage =
+          base64.decode(_src(attributes)!.split("base64,")[1].trim());
       precacheImage(
         MemoryImage(decodedImage),
         context.buildContext,
@@ -57,7 +66,8 @@ ImageRender base64ImageRender() => (context, attributes, element) {
         decodedImage,
         frameBuilder: (ctx, child, frame, _) {
           if (frame == null) {
-            return Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
+            return Text(_alt(attributes) ?? "",
+                style: context.style.generateTextStyle());
           }
           return child;
         },
@@ -71,7 +81,9 @@ ImageRender assetImageRender({
     (context, attributes, element) {
       final assetPath = _src(attributes)!.replaceFirst('asset:', '');
       if (_src(attributes)!.endsWith(".svg")) {
-        return SvgPicture.asset(assetPath, width: width ?? _width(attributes), height: height ?? _height(attributes));
+        return SvgPicture.asset(assetPath,
+            width: width ?? _width(attributes),
+            height: height ?? _height(attributes));
       } else {
         return Image.asset(
           assetPath,
@@ -79,7 +91,8 @@ ImageRender assetImageRender({
           height: height ?? _height(attributes),
           frameBuilder: (ctx, child, frame, _) {
             if (frame == null) {
-              return Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
+              return Text(_alt(attributes) ?? "",
+                  style: context.style.generateTextStyle());
             }
             return child;
           },
@@ -97,74 +110,133 @@ ImageRender networkImageRender({
 }) =>
     (context, attributes, element) {
       final src = mapUrl?.call(_src(attributes)) ?? _src(attributes)!;
-      Completer<Size> completer = Completer();
-      if (context.parser.cachedImageSizes[src] != null) {
-        completer.complete(context.parser.cachedImageSizes[src]);
-      } else {
-        Image image = Image.network(src, frameBuilder: (ctx, child, frame, _) {
-          if (frame == null) {
-            if (!completer.isCompleted) {
-              completer.completeError("error");
-            }
-            return child;
-          } else {
-            return child;
-          }
-        });
+      // Completer<Size> completer = Completer();
+      // if (context.parser.cachedImageSizes[src] != null) {
+      //   completer.complete(context.parser.cachedImageSizes[src]);
+      // } else {
+      //   Image image = Image.network(src, frameBuilder: (ctx, child, frame, _) {
+      //     if (frame == null) {
+      //       if (!completer.isCompleted) {
+      //         completer.completeError("error");
+      //       }
+      //       return child;
+      //     } else {
+      //       return child;
+      //     }
+      //   });
 
-        ImageStreamListener? listener;
-        listener = ImageStreamListener((ImageInfo imageInfo, bool synchronousCall) {
-          var myImage = imageInfo.image;
-          Size size = Size(myImage.width.toDouble(), myImage.height.toDouble());
-          if (!completer.isCompleted) {
-            context.parser.cachedImageSizes[src] = size;
-            completer.complete(size);
-            image.image.resolve(ImageConfiguration()).removeListener(listener!);
-          }
-        }, onError: (object, stacktrace) {
-          if (!completer.isCompleted) {
-            completer.completeError(object);
-            image.image.resolve(ImageConfiguration()).removeListener(listener!);
-          }
-        });
+      //   ImageStreamListener? listener;
+      //   listener =
+      //       ImageStreamListener((ImageInfo imageInfo, bool synchronousCall) {
+      //     var myImage = imageInfo.image;
+      //     Size size = Size(myImage.width.toDouble(), myImage.height.toDouble());
+      //     if (!completer.isCompleted) {
+      //       context.parser.cachedImageSizes[src] = size;
+      //       completer.complete(size);
+      //       image.image.resolve(ImageConfiguration()).removeListener(listener!);
+      //     }
+      //   }, onError: (object, stacktrace) {
+      //     if (!completer.isCompleted) {
+      //       completer.completeError(object);
+      //       image.image.resolve(ImageConfiguration()).removeListener(listener!);
+      //     }
+      //   });
 
-        image.image.resolve(ImageConfiguration()).addListener(listener);
+      //   image.image.resolve(ImageConfiguration()).addListener(listener);
+      // }
+      // print(context.parser.cachedImageSizes[src]);
+
+      double? __width = 343;
+      double? __height =
+          ((_height(attributes) ?? 0.0) / (_width(attributes) ?? 0.0)) *
+              __width;
+      if (_width(attributes) == null) {
+        __width = width ?? _width(attributes);
+        __height = height ?? _height(attributes);
       }
-
-      return FutureBuilder<Size>(
-        future: completer.future,
-        initialData: context.parser.cachedImageSizes[src],
-        builder: (BuildContext buildContext, AsyncSnapshot<Size> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            ImgRenderComplete.count++;
-            ImgRenderComplete.fn?.call(ImgRenderComplete.count);
-          }
-          if (snapshot.hasData) {
-            return Container(
-              constraints: BoxConstraints(maxWidth: width ?? _width(attributes) ?? snapshot.data!.width, maxHeight: (width ?? _width(attributes) ?? snapshot.data!.width) / _aspectRatio(attributes, snapshot)),
-              child: AspectRatio(
-                aspectRatio: _aspectRatio(attributes, snapshot),
-                child: Image.network(
-                  src,
-                  headers: headers,
-                  width: width ?? _width(attributes) ?? snapshot.data!.width,
-                  height: height ?? _height(attributes),
-                  frameBuilder: (ctx, child, frame, _) {
-                    if (frame == null) {
-                      return altWidget?.call(_alt(attributes)) ?? Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
-                    }
-                    return child;
-                  },
-                ),
+      // ImgRenderComplete.count++;
+      // Future.delayed(Duration(milliseconds: 300), () {
+      //   ImgRenderComplete.fn?.call(ImgRenderComplete.count);
+      // });
+      return Container(
+        // child: loadingWidget?.call(),
+        child: src == null
+            ? loadingWidget?.call()
+            : Image.network(
+                src,
+                width: __width,
+                height: __height,
               ),
-            );
-          } else if (snapshot.hasError) {
-            return altWidget?.call(_alt(attributes)) ?? Text(_alt(attributes) ?? "", style: context.style.generateTextStyle());
-          } else {
-            return loadingWidget?.call() ?? const CircularProgressIndicator();
-          }
-        },
+        width: __width,
+        height: __height,
       );
+      // return FutureBuilder<Size>(
+      //   future: completer.future,
+      //   initialData: context.parser.cachedImageSizes[src],
+      //   builder: (BuildContext buildContext, AsyncSnapshot<Size> snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.done) {
+      //       ImgRenderComplete.count++;
+      //       ImgRenderComplete.fn?.call(ImgRenderComplete.count);
+      //     }
+      //     double __width = 343;
+      //     double? __height =
+      //         ((_height(attributes) ?? 0.0) / (_width(attributes) ?? 0.0)) *
+      //             __width;
+      //     if (_width(attributes) == null) {
+      //       __width = width ?? _width(attributes) ?? snapshot.data!.width;
+      //       __height = height ?? _height(attributes);
+      //     }
+
+      //     // return Container(
+      //     //   child: src == loadingWidget?.call()
+      //     //       ? null
+      //     //       : Image.network(
+      //     //           src,
+      //     //           width: __width,
+      //     //           height: __height,
+      //     //         ),
+      //     //   // color: Colors.red,
+      //     //   width: __width,
+      //     //   height: __height,
+      //     // );
+      //     if (snapshot.hasData) {
+      //       return Container(
+      //         constraints: BoxConstraints(
+      //             maxWidth: width ?? _width(attributes) ?? snapshot.data!.width,
+      //             maxHeight:
+      //                 (width ?? _width(attributes) ?? snapshot.data!.width) /
+      //                     _aspectRatio(attributes, snapshot)),
+      //         child: AspectRatio(
+      //           aspectRatio: _aspectRatio(attributes, snapshot),
+      //           child: Image.network(
+      //             src,
+      //             headers: headers,
+      //             width: width ?? _width(attributes) ?? snapshot.data!.width,
+      //             height: height ?? _height(attributes),
+      //             frameBuilder: (ctx, child, frame, _) {
+      //               if (frame == null) {
+      //                 return altWidget?.call(_alt(attributes)) ??
+      //                     Text(_alt(attributes) ?? "",
+      //                         style: context.style.generateTextStyle());
+      //               }
+      //               return child;
+      //             },
+      //           ),
+      //         ),
+      //       );
+      //     } else if (snapshot.hasError) {
+      //       return altWidget?.call(_alt(attributes)) ??
+      //           Text(_alt(attributes) ?? "",
+      //               style: context.style.generateTextStyle());
+      //     } else {
+      //       return Container(
+      //         child: loadingWidget?.call() ?? const CircularProgressIndicator(),
+      //         width: __width,
+      //         height: __height,
+      //       );
+      //     }
+      //   },
+      // );
     };
 
 ImageRender svgDataImageRender() => (context, attributes, element) {
@@ -208,21 +280,28 @@ String? _alt(Map<String, String> attributes) {
 
 double? _height(Map<String, String> attributes) {
   final heightString = attributes["height"];
-  return heightString == null ? heightString as double? : double.tryParse(heightString);
+  return heightString == null
+      ? heightString as double?
+      : double.tryParse(heightString);
 }
 
 double? _width(Map<String, String> attributes) {
   final widthString = attributes["width"];
-  return widthString == null ? widthString as double? : double.tryParse(widthString);
+  return widthString == null
+      ? widthString as double?
+      : double.tryParse(widthString);
 }
 
-double _aspectRatio(Map<String, String> attributes, AsyncSnapshot<Size> calculated) {
+double _aspectRatio(
+    Map<String, String> attributes, AsyncSnapshot<Size> calculated) {
   final heightString = attributes["height"];
   final widthString = attributes["width"];
   if (heightString != null && widthString != null) {
     final height = double.tryParse(heightString);
     final width = double.tryParse(widthString);
-    return height == null || width == null ? calculated.data!.aspectRatio : width / height;
+    return height == null || width == null
+        ? calculated.data!.aspectRatio
+        : width / height;
   }
   return calculated.data!.aspectRatio;
 }
